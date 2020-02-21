e9ead335a0ffab4fb0ad8e340402a44701772f0f
iqbalfebrian <61312410+iqbalfebrian@users.noreply.github.com>
2020-02-21 18:37:14 +0900

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,14 @@
+
+
+for (var i = 0; i<document.querySelectorAll(".drum").length; i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+    var x = new Audio("C:\Users\IQBAL FEBRIAN\Pictures\programming\web development\advanced\Drum Kit Starting Files\sounds\tom-1.mp3");
+    x.play();
+
+
+    }
+
+});
+}
